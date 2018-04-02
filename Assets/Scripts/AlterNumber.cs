dd306799bd13f819ed833086d1bfcc0204ce971c
Nic <aphemra@gmail.com>
2018-04-02 05:42:03 -0400

    Initial Project Files
    Includes code for formatting large numbers nicely.

---
+++ Assets/Scripts/AlterNumber.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlterNumber : MonoBehaviour {
+
+    public float numberToStartAt = 100.000f;
+    public Text numberToDisplay;
+
+    public BigNumberFormatter bigNumberFormatter;
+
+    private double number;
+    
+	void Start ()
+    {
+        number = numberToStartAt;
+
+        numberToDisplay.text = bigNumberFormatter.format(number);
+	}
+	
+	void Update () {
+        number++;
+        numberToDisplay.text = bigNumberFormatter.format(number);
+    }
+
+    public void add(float numberToIncreaseBy)
+    {
+        number += numberToIncreaseBy;
+    }
+
+    public void subtract(float numberToDecreaseBy)
+    {
+        if (number - numberToDecreaseBy <= 0)
+            return; // Notify player they can't do this
+
+        number -= numberToDecreaseBy;
+    }
+}
---
+++ Assets/Scripts/BigNumberFormatter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigNumberFormatter : MonoBehaviour {
+    
+    private int formatIndex;
+    private int numberSize;
+    private string[] fullNameFormatSuffixes = new string[] { "", "Thousand", "Million", "Billion", "Trillion", "Quadrillion", "Quintillion", "Sextillion", "Septillion", "Octillion", "Nonillion", "Decillion" };
+    private string[] shortNameFormatSuffixes = new string[] { "", "K", "M", "B", "T", "Qa", "Qi", "Sx", "Sp", "Oc", "No", "De" };
+
+    void Update()
+    {
+        formatIndex = GameManager.GlobalFormatIndex;
+    }
+
+    public string format (double toConvert)
+    {
+        numberSize = 0;
+
+        while( toConvert >= 1000)
+        {
+            toConvert /= 1000;
+            numberSize++;
+        }
+
+        switch (formatIndex)
+        {
+            case 0:
+                return format(toConvert, fullNameFormatSuffixes[numberSize]);
+            case 1:
+                return format(toConvert, shortNameFormatSuffixes[numberSize]);
+            default:
+                Debug.Log("NoFormatError: No formatting selected. Check the BigNumberFormatter script.");
+                return "ERROR";
+        }
+    } 
+
+    private string format(double toConvert, string suffix)
+    {
+        return string.Format("${0:#,##0.00} {1}", toConvert, suffix);
+    }
+}
